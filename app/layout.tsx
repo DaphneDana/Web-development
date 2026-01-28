d028ce1a86237f36d36ee0f2646793c74e6a08e5
DaphneDana <nakamyukadaphne62@gmail.com>
2026-01-28 23:26:41 +0300

    added the footer to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +15,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "RecipeShare - Discover and Share Amazing Recipes",
-  description: "A community platform for sharing and discovering delicious recipes. Browse our collection of recipes from breakfast to dessert!",
+  description:
+    "A community platform for sharing and discovering delicious recipes. Browse our collection of recipes from breakfast to dessert!",
 };
 
 export default function RootLayout({
@@ -27,8 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex min-h-screen flex-col">
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
 }
+
