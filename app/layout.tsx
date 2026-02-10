0c256b2087b12ec401b414f6ff9b446fb4b8c55a
DaphneDana <nakamyukadaphne62@gmail.com>
2026-02-10 23:26:32 +0300

    fixed the layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import ThemeProvider from "@/components/ThemeProvider";
-
+{/*LAYOUT COMPONENT*/}
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
