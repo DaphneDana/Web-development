2a1ce8f5fbbb3d7d4f75efdff303c933afa2a812
DaphneDana <nakamyukadaphne62@gmail.com>
2026-02-12 23:02:52 +0300

    recipe page

--- app/recipe/[id]/page.tsx
+++ app/recipe/[id]/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Navbar from '@/components/Navbar';
 import { mockRecipes } from '@/lib/mockData';
-
+{/*recipe page*/}
 interface RecipePageProps {
   params: {
     id: string;
